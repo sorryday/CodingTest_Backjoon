3bc00daadba8ad641d549b50340e200e86594ec8
sorryday <rptqoxmf5886@naver.com>
2022-05-15 23:03:10 +0900

    잃어버린 괄호 - 1541

--- miss_bracket.java
+++ miss_bracket.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 class miss_bracket {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
