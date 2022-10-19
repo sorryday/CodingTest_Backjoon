1379169e0f96e53d687ec51551fe1327f557be23
sorryday <rptqoxmf5886@naver.com>
2022-10-19 23:48:58 +0900

    투명 - 1531

---
+++ implementation/Transparency.java
@@ -0,0 +1,45 @@
+package implementation;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Transparency {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int result = 0;
+
+        int[][] map = new int[101][101];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int j = x1; j <= x2; j++) {
+                for (int k = y1; k <= y2; k++) {
+                    map[j][k] += 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                if (map[i][j] > M) {
+                    result += 1;
+                }
+            }
+        }
+
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
