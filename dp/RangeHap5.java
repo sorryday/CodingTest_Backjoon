9dcc1a77312f673e45387e44e6049bbba50071d4
sorryday <rptqoxmf5886@naver.com>
2023-02-09 05:01:22 +0900

    구간 합 구하기 5 - 11660

--- dp/RangeHap5.java
+++ dp/RangeHap5.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class RangeHap5 {
 
     static int N, M;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -31,12 +30,7 @@ public class RangeHap5 {
         dp[1][1] = map[1][1];
 
         for(int i = 1; i <= N; i++) {
-//            if (i != 1) {
-//                dp[i][1] = dp[i - 1][N] + map[i][1];
-//            }
-
             for (int j = 1; j <= N; j++) {
-//                dp[i][j] = dp[i][j - 1] + map[i][j];
                 dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + map[i][j];
             }
 
@@ -54,15 +48,6 @@ public class RangeHap5 {
 
             bw.write(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 -1][y1 - 1] + "\n");
         }
-
-//        //출력
-//        for (int i = 1; i <= N; i++) {
-//            for (int j = 1; j <= N; j++) {
-//                bw.write(dp[i][j] + " ");
-//            }
-//            bw.write("\n");
-//        }
-
 
         bw.flush();
         bw.close();
