7fb3c4db2bbc2c9eb3827dd6b2af4a7d668e8274
sorryday <rptqoxmf5886@naver.com>
2023-02-09 04:50:15 +0900

    구간 합 구하기 5 - 11660

---
+++ dp/RangeHap5.java
@@ -0,0 +1,71 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class RangeHap5 {
+
+    static int N, M;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());	// 맵 크기
+        M = Integer.parseInt(st.nextToken());	// 구해야하는 테스트 케이스 개수
+
+        int[][] map = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // dp 배열을 미리 만들어두기
+        int[][] dp = new int[N + 1][N + 1];
+        dp[1][1] = map[1][1];
+
+        for(int i = 1; i <= N; i++) {
+//            if (i != 1) {
+//                dp[i][1] = dp[i - 1][N] + map[i][1];
+//            }
+
+            for (int j = 1; j <= N; j++) {
+//                dp[i][j] = dp[i][j - 1] + map[i][j];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + map[i][j];
+            }
+
+        }
+
+        // 테스트 케이스 읽어오기 시작
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            bw.write(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 -1][y1 - 1] + "\n");
+        }
+
+//        //출력
+//        for (int i = 1; i <= N; i++) {
+//            for (int j = 1; j <= N; j++) {
+//                bw.write(dp[i][j] + " ");
+//            }
+//            bw.write("\n");
+//        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
